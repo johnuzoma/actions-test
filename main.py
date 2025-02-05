17e078659d0c3bdef2c9489c499dd9c19bcda327
John Uzoma <bobonnaaa@gmail.com>
2025-02-05 12:17:30 +0000

    second commit

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 import pandas as pd
 
-url = 'https://www.dublincity.ie/residential/parks/dublin-city-parks/pitch-playability'
-data = requests.get(url)
+if __name__ == '__main__':
+    url = 'https://www.dublincity.ie/residential/parks/dublin-city-parks/pitch-playability'
+    data = requests.get(url)
 
-my_data = []
-html = BeautifulSoup(data.text, 'html.parser')
-rows = html.select('tr')
+    my_data = []
+    html = BeautifulSoup(data.text, 'html.parser')
+    rows = html.select('tr')
 
-for row in rows[1:]:
-    park = row.select('td')[0].get_text()
-    status_on = row.select('td')[1].get_text()
-    my_data.append({
-        "Parks": park,
-        "Status": status_on
-    })
+    for row in rows[1:]:
+        park = row.select('td')[0].get_text()
+        status_on = row.select('td')[1].get_text()
+        my_data.append({
+            "Parks": park,
+            "Status": status_on
+        })
 
-df = pd.DataFrame(my_data)
-df['Status'] =  df['Status'].replace('\xa0', 'OFF')
+    df = pd.DataFrame(my_data)
+    df['Status'] =  df['Status'].replace('\xa0', 'OFF')
 
-df.to_csv('data.csv')
-"""
-f = open('PitchPlayability.txt', 'x')
+    df.to_csv('data.csv')
+    """
+    f = open('PitchPlayability.txt', 'x')
 
-f.write(df.to_string())
+    f.write(df.to_string())
 
-f.close()
-"""
+    f.close()
+    """
